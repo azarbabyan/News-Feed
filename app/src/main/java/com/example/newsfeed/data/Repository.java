f9736000fcd30694da4d5c840c76e69f40628fec
azarbabyan <arturzarbabyan@gmail.com>
2019-06-20 22:46:05 +0400

    initial commit

---
+++ app/src/main/java/com/example/newsfeed/data/Repository.java
@@ -0,0 +1,5 @@
+package com.example.newsfeed.data;
+
+public class Repository {
+
+}
---
+++ app/src/main/java/com/example/newsfeed/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.newsfeed.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.newsfeed.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
